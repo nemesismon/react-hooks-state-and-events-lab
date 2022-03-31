5ed3ee2c77e5dddd4e63904dbd7a06c12b9835ea
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-03-31 13:37:28 -0400

    TC

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  
+  const [isAdded, setIsAdded] = useState(false);
+
+  function addItem () {
+    setIsAdded((isAdded) => !isAdded);
+}
+
+  const inCart = isAdded ? "in-cart" : "";
+  const color = isAdded ? "pink" : "yellow";
+  const buttonText = isAdded ? "Remove From Cart" : "Add To Cart";
+
   return (
-    <li className="">
+    <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" style={{background: color}} onClick={addItem}>{buttonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleFilter (event) {
+    setSelectedCategory(event.target.value)
+    setSelectedCategory((selectedCategory) => selectedCategory)
+    console.log(selectedCategory);
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
